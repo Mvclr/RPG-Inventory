9d7cdeccc13916ba803d36883bdf258d8e8f4091
Manoel <mvclr1@gmail.com>
2025-12-05 16:11:55 -0300

    Interfaces of the itens

--- src/models/ItemInterfaces.ts
+++ src/models/ItemInterfaces.ts
@@ -1,11 +1,30 @@
-import { Interface } from "readline";
 import { ItemRarity, ItemType } from "./ItensTypes";
 
-export interface IbaseItem {
+export interface BaseItem {
     name: string;
     type: ItemType;
-    rearity: ItemRarity;
+    rarity: ItemRarity;
     weight: number;
     value:number;
 }
 
+export interface Weapon extends BaseItem{
+    type: ItemType.WEAPON;
+    damage: number;
+    range: number;
+    durability: number;
+
+}
+
+export interface Armor extends BaseItem{
+    type: ItemType.ARMOR;
+    defense: number;
+    hpBonus: number;
+    durability: number;
+}
+
+export interface Potion extends BaseItem{
+    type: ItemType.POTION;
+    effect: string;
+    duration: number;
+}
